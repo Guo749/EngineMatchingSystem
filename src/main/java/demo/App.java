9f41ad35603e58568adf9c4292b8b3ed966fa0f9
Guo749 <venturekwok@gmail.com>
2022-03-19 09:04:59 -0400

    establish C/S model, now need to process XML & Database

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -1,7 +1,11 @@
 package demo;
 
+/**
+ * App entry point
+ */
 public class App {
     public static void main(String[] args) {
         Server server = new Server(12345);
+        server.run();
     }
 }
--- src/main/java/demo/Server.java
+++ src/main/java/demo/Server.java
@@ -2,63 +2,148 @@ package demo;
 
 import java.io.BufferedInputStream;
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server
 {
-    //initialize socket and input stream
-    private Socket socket   = null;
-    private ServerSocket server   = null;
-    private DataInputStream in       =  null;
+    /* server socket */
+    private ServerSocket serverSocket   = null;
 
-    // constructor with port
-    public Server(int port)
-    {
-        // starts server and waits for a connection
-        try
-        {
-            server = new ServerSocket(port);
+    /* the stream we read from the user */
+    private DataInputStream in    =  null;
+
+    public Server(int port) {
+        try {
+            this.serverSocket = new ServerSocket(port);
             System.out.println("Server started");
-
-            System.out.println("Waiting for a client ...");
-
-            socket = server.accept();
-            System.out.println("Client accepted");
-
-            // takes input from the client socket
-            in = new DataInputStream(
-                new BufferedInputStream(socket.getInputStream()));
-
-            String line = "";
-
-            // reads message from client until "Over" is sent
-            while (!line.equals("Over"))
-            {
-                try
-                {
-                    line = in.readLine();
-
-                    System.out.println(line);
-
+        }catch (IOException e){
+            System.out.println("IO Exception");
+            try {
+                if(this.serverSocket!= null) {
+                    this.serverSocket.close();
                 }
-                catch(IOException i)
-                {
-                    System.out.println(i);
-                }
+            } catch (IOException ex) {
+                System.out.println("cannot close server socket");
             }
-            System.out.println("Closing connection");
-
-            // close connection
-            socket.close();
-            in.close();
-        }
-        catch(IOException i)
-        {
-            System.out.println(i);
         }
     }
 
+    /**
+     * Constantly receive connection from the client
+     * and read entire request
+     */
+    public void run(){
+        while(true) {
+            Socket client = null;
+            try {
+                /* step1: accept the connection */
+                client = this.serverSocket.accept();
+                System.out.println("Client accepted");
+
+                /* step2: read the content, both len & message */
+                this.in = new DataInputStream(
+                    new BufferedInputStream(client.getInputStream()));
+
+                int xmlLen = readLine();
+
+                /* step3: process it accordingly */
+                String xml = readNum(xmlLen);
+
+                processXML(xml);
+
+                /* step4: write it back the result */
+                writeToClient(client, "what I receive " + xmlLen);
+
+            } catch (IOException | IllegalArgumentException e) {
+                e.printStackTrace();
+            }finally {
+                if(client != null){
+                    try {
+                        client.close();
+                    } catch (IOException e) {
+                        System.out.println("cannot close the client");
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Receive xml from the client, and try to do that accordingly
+     */
+    private void processXML(String xml) {
+        System.out.println("I will do xml");
+    }
+
+    /**
+     * Used to read the first line of XML, the length of the xml
+     *
+     * @return the first line of the stream
+     */
+    private Integer readLine() throws IOException, IllegalArgumentException {
+        int c;
+        int counter = 0;
+        StringBuilder res = new StringBuilder();
+
+        while(true){
+            if(counter == 11){
+                throw new IllegalArgumentException("bad request, cannot get length");
+            }
+
+            c = this.in.read();
+            if(c == '\n'){
+                break;
+            }else if(Character.isDigit(c)){
+                char ch = (char) c;
+                res.append(ch);
+            }else{
+                throw new IOException("bad format for length in xml");
+            }
+
+            counter++;
+        }
+
+        try {
+            return Integer.parseInt(res.toString());
+        }catch(NumberFormatException e){
+            throw new IllegalArgumentException("bad format for content length");
+        }
+    }
+
+    /**
+     * Used to read num bytes from client
+     * @param num # of bytes to read
+     * @return the content
+     */
+    private String readNum(int num) throws IOException {
+        int c;
+        StringBuilder res = new StringBuilder();
+
+        while(num != 0) {
+            c = this.in.read();
+            res.append(((char)c));
+            num--;
+        }
+
+        return res.toString();
+    }
+
+    /**
+     * Wrapper for write function
+     * @param client the client to pass the information
+     * @param content the message to deliver
+     */
+    public void writeToClient(Socket client, String content){
+        try {
+            DataOutputStream dos = new DataOutputStream(client.getOutputStream());
+            dos.writeUTF(content);
+        } catch (IOException e) {
+            System.out.println("write error");
+        }
+
+    }
 
 }
